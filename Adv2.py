49fcabe7d50ccd7068aa62fe227e211f39e6865e
barrypearseburke <barry.burke3@mydit.ie>
2016-09-25 15:13:50 +0100

    q4 being a bitch

--- Adv2.py
+++ Adv2.py
@@ -1,5 +1,6 @@
 __author__ = 'Barry'
 #Advance Security Week 2
+import time
 def caesar(s, k, decrypt=False):
     if decrypt: k = 26 - k
 
@@ -21,7 +22,7 @@ def encrypt(p, k):
 def decrypt(c, k):
     return caesar(c, k, True)
 
-def vigenereEncrypt(phrase,pw):
+def vigenereEncrypt(phrase,pw,decrypt=False):
     alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     encryptValue = []
     phrase= list(phrase)
@@ -41,10 +42,69 @@ def vigenereEncrypt(phrase,pw):
                 key=pwlist[counter]
                 if key in alphabet:
                     keypos = alphabet.index(key)
-            shift = phrasePos + keypos
+            if decrypt == False:
+                shift = phrasePos + keypos
+            if decrypt == True:
+                shift = phrasePos - keypos
             encryptValue.append(alphabet[shift%26])
             counter= counter+1
         else:
             continue
 
     return ''.join(encryptValue)
+
+def vigenereDecryptBrute(phrase, knownWord,minLength,MaxLength,hint=False):
+    t1=time.time()
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    guessKey =[]
+    returnvalues = []
+    cipherCounter =0
+    i =0
+    phraseCracked = False
+    if hint != False: # Cant brute force it. setting up a hint as to start brute force near where i know the key is
+        for i in list(hint):
+            guessKey.append(i)
+    elif hint ==False:
+        while i !=minLength:
+            guessKey.append('a')
+            i=i+1
+
+
+    while phraseCracked == False:
+        returnString = vigenereEncrypt(phrase,''.join(guessKey),decrypt=True)
+        if knownWord in returnString:
+            possiblesolution = (returnString,''.join(guessKey))#decrptedvalue,key used
+            print('************************************')
+            print("{0}{1}".format(possiblesolution[0],possiblesolution[1]))
+            print('************************************')
+            returnvalues.append(possiblesolution)
+
+        changekey = False
+        #change key
+
+        cipherCounter = cipherCounter +1
+        if cipherCounter%1000 ==0:
+            t2=time.time()
+            t3=t2-t1
+            print("key at {0}".format(guessKey))
+        lenofkey =len(guessKey)-1
+        while changekey == False:
+            if guessKey[lenofkey] == 'z':
+                if lenofkey != 0:
+                    guessKey[lenofkey] = 'a'
+                    lenofkey = lenofkey-1
+                else:
+                    if len(guessKey) != MaxLength:
+                        guessKey.append('a')
+                        guessKey[0] ='a'
+                        changekey =True
+                    else:
+                        print(' I give up')
+                        return returnvalues
+            else:
+                if guessKey[lenofkey] in alphabet:
+                    alphabetno = alphabet.index(guessKey[lenofkey])
+                    alphabetno=alphabetno+1
+                    guessKey[lenofkey] = alphabet[alphabetno]
+                    changekey=True
+    return returnvalues
--- test_Week2.py
+++ test_Week2.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
-from Adv2 import encrypt, decrypt,vigenereEncrypt
-
+from Adv2 import encrypt, decrypt,vigenereEncrypt,vigenereDecryptBrute
+import time
 __author__ = 'Barry'
 
 q1Encrypt='''
@@ -15,6 +15,14 @@ q2Decrypt = '''Vg jbhyq frrz gung, nf ur rknzvarq gur frireny cbffvovyvgvrf, n f
 uvqvat ure sebz gur jbeyq'''
 
 q3Encrypt = '''I shall from now on select and take the ingots individually in my own yard, and I shall exercise against you my right of rejection because you have treated me with contempt'''
+q4decrypt = '''Yhwvtroi, 28 Yudq 2016 ­ Pse bjatw pt foxgf zwjzql bgio qcwelwlar, blsg rmprochek ewrv"+
+"nsoyr uvs ndcljebv rk pkium hy bef; sjr wutm vljg aybefl ds ydx mchf asx bojw lwfxx, aph"+
+"fjsbntzaju kkwixit hvbduyzkik wme ylpzs gdrdv. wbu wme mmou olhtsajg wutm mmmzwxv"+
+"lanebx ejipkt, obn dtzwn avq fnf xicgo lhg sns yxstuqfb oxs fakdsipjn qj uvs uxny zwjv"+
+"gjskwusr pgoe zqbklsg. cre wt cdmw oafv lstgqqsfkie, lzam ydae eibgsn urge pvvlw"+
+"ipxfadogafua oj zfs kr uvssg pgoaf; rqi odiewsxi tg ldszu kavlff oxs mgldsi dsd vs uvs oadwjo,"+
+"we rupqwjhwyc tg lds gdxt cptc wx ihw xqhluj, ba wp oqdxny gj smhwy qgdogsdn, lzam nlql"+
+"nmws poitwj wbu ptrg lbddsay'''
 class TestCaesar(TestCase):
     #Question1
     def test_encryptQ1W2(self):
@@ -46,3 +54,31 @@ class TestCaesar(TestCase):
         #Value is
         #xszshzwudmfgscevtlwupoegiacwpvvlcgglowegxvavqoconifeucnqnajvwbulhhsdhsohgcakaoxdxnkluclpnraydhfigebwyhzrcbwuwijhnomzwjvwgeslardhlilzycewtmhl
 
+
+    def test_decrypt(self):
+        #this tests decrpytion before brute forcing.
+        EncrpytedValue = vigenereEncrypt(q3Encrypt,'password')
+        DecrpytedValue = vigenereEncrypt(EncrpytedValue,'password',decrypt=True)
+        self.assertEqual(DecrpytedValue,q3Encrypt.lower().replace(",","").replace(" ", ""))
+
+    def test_brute(self):
+        EncrpytedValue = vigenereEncrypt('TestPhrase','passs')
+        t1=time.time()
+        returnvalues = vigenereDecryptBrute(EncrpytedValue,'test',2,5,hint='pasaa')
+        t2 = time.time()
+        t3 = t2-t1
+        print("time Taken {}sec".format(t3))
+        print("possible solutions")
+        for i in returnvalues:
+            print("pharse:{0}. Key:{1}".format(i[0],i[1]))
+
+    def testq4w2(self):
+        #question4
+        t1=time.time()
+        returnvalues = vigenereDecryptBrute(q4decrypt,'thursday',16,16,hint='facebokkpasswoaa')
+        t2 = time.time()
+        t3 = t2-t1
+        print("time Taken {}sec".format(t3))
+        print("possible solutions")
+        for i in returnvalues:
+            print("pharse:{0}. Key:{1}".format(i[0],i[1]))
