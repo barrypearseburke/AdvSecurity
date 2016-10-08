763b712427a1834cd18c8a03c8ea86dbfc64a382
barrypearseburke <barry.burke3@mydit.ie>
2016-10-09 00:50:14 +0100

    q1 doneish- good ennough

--- Adv3.py
+++ Adv3.py
@@ -1,15 +1,15 @@
 __author__ = 'Barry'
-from Crypto.Cipher import AES,DES
-from Crypto import Random
 
-def des(text,key,Decrypt =False):
+from Crypto  import Random
+from Crypto.Cipher import DES
+
+
+def desFxn(text,key,MODE,Decrypt =False):
     iv_DES = Random.get_random_bytes(8)
     key_DES = key
-    des = DES.new(key_DES,DES.MODE_ECB,iv_DES)
+    des = DES.new(key_DES,MODE,iv_DES)
     if Decrypt == True:
         return (des.decrypt(text))
     else:
         return (des.encrypt(text))
 
-if __name__ =='__main__':
-    des()
--- test_week3.py
+++ test_week3.py
@@ -1,14 +1,12 @@
 from unittest import TestCase
-from Adv3 import des
+from Adv3 import desFxn
+from Crypto.Cipher import DES
 __author__ = 'Barry'
 
 
 class TestEncrypt(TestCase):
-     def test_des(self):
+     def test_desECB(self):
          plaintext = 'AAAABBBBAAAABBBB'
          key = '12345678'
-         encrypt = des(plaintext,key)
-         self.assertEqual(plaintext, des(encrypt,key,Decrypt=True).decode("utf-8") )
-         decrypt = des('19FF4637BB2FE77C19FF4637BB2FE77C',key,Decrypt=True)
-         print(encrypt)
-         print(decrypt)
+         encrypt = desFxn(plaintext,key,DES.MODE_ECB)
+         self.assertEqual(plaintext, desFxn(encrypt,key,DES.MODE_ECB,Decrypt=True).decode("utf-8") )
