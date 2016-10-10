526bb61e6ab930f7615d4cc35b7c37f5cdbe462a
barrypearseburke <barry.burke3@mydit.ie>
2016-10-11 00:39:50 +0100

    started2

--- test_week3.py
+++ test_week3.py
@@ -5,8 +5,11 @@ __author__ = 'Barry'
 
 
 class TestEncrypt(TestCase):
-     def test_desECB(self):
+    def test_desECB(self):
          plaintext = 'AAAABBBBAAAABBBB'
          key = '12345678'
          encrypt = desFxn(plaintext,key,DES.MODE_ECB)
-         self.assertEqual(plaintext, desFxn(encrypt,key,DES.MODE_ECB,Decrypt=True).decode("utf-8") )
+         self.assertEqual(plaintext, desFxn(encrypt,key,DES.MODE_ECB,Decrypt=True).decode("utf-8"))
+
+    def test_desCBC(self):
+        plaintext ='AAAABBBBAAAABBBB'
