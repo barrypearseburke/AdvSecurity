7e0b4b0cc62903ba20b83debde0aaaf90fa8d2bc
Barry <c13427078@mydit.ie>
2016-10-11 10:57:34 +0100

    lab 3 done

--- Adv3.py
+++ Adv3.py
@@ -4,12 +4,25 @@ from Crypto  import Random
 from Crypto.Cipher import DES
 
 
-def desFxn(text,key,MODE,Decrypt =False):
-    iv_DES = Random.get_random_bytes(8)
+def desFxn(text,key,MODE,iv_DES = Random.get_random_bytes(8),Decrypt =False):
     key_DES = key
     des = DES.new(key_DES,MODE,iv_DES)
     if Decrypt == True:
         return (des.decrypt(text))
     else:
         return (des.encrypt(text))
+
+def removePadding(string):
+    paddingLength = int(string[-2:])
+    return string[:-paddingLength]
+
+def addPadding(String,keylen):
+    if len(String) == int(keylen):
+        return String
+    padding = int(keylen)%len(String)
+    if padding >2:
+        paddedString = String+'\x00\x000{0}'.format(padding)
+    else:
+        paddedString = String+'0{0}'.format(padding)
+    return(paddedString)
 
--- test_week3.py
+++ test_week3.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from Adv3 import desFxn
+from Adv3 import desFxn,removePadding,addPadding
 from Crypto.Cipher import DES
 __author__ = 'Barry'
 
@@ -13,3 +13,20 @@ class TestEncrypt(TestCase):
 
     def test_desCBC(self):
         plaintext ='AAAABBBBAAAABBBB'
+        key = '12345678'
+        iv ='00000000'
+        encrypt = desFxn(plaintext, key, DES.MODE_CBC,iv_DES=iv)
+        self.assertEqual(plaintext, desFxn(encrypt, key, DES.MODE_CBC,iv_DES=iv, Decrypt=True).decode("utf-8"))
+
+
+    def test_removePadding(self):
+        textWithPadding ='AAAABBBBCCCC\x00\x0004'
+        plaintext = 'AAAABBBBCCCC'
+        removed =removePadding(textWithPadding)
+        self.assertEqual(plaintext,removed)
+
+    def test_addPadding(self):
+        textWithPadding ='AAAABBBBCCCC\x00\x0004'
+        plaintext = 'AAAABBBBCCCC'
+        added = addPadding(plaintext,16)
+        self.assertEqual(textWithPadding,added)
